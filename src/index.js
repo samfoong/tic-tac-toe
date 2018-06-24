76fccfd9b3c1c78f59ebfb4590fa12efc45a6353
samfoong <samfoong88@gmail.com>
2018-06-24 15:38:57 +1000

    Add constructor and set initial state to contain an array with 9nulls or 9 squares

--- src/index.js
+++ src/index.js
@@ -22,6 +22,13 @@ class Square extends React.Component {
   
   // Change the code to pass a value prop to the Square
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+    
     renderSquare(i) {
       return <Square value={i} />;  
     }
